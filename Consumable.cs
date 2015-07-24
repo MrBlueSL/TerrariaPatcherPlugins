7d62e04509b7e5555c9577a80c5ad4ee934775e7
MrBlueSL <tbecker1996@gmail.com>
2015-07-24 00:24:21 -0500

    Plugins
    Plugins I've made so far. More to come eventually

---
+++ Consumable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PluginLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace MrBlueSLPlugins
+{
+    public class Consumable : MarshalByRefObject, IPluginChatCommand, IPluginItemSetDefaults
+    {
+        private bool im;
+        private bool i = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].consumable;
+        private string itm = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].name;
+        public bool OnChatCommand(string command, string[] args)
+        {
+           /* string itm = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].name;
+            if (command != "consumable") return false;
+            if (command == "consumable" && i == true)
+            {
+                im = false;
+                Main.NewText(itm + " will not consume on use");
+            }
+            else
+            {
+                im = true;
+                Main.NewText(itm + " will consume on use");
+            }*/
+            return true;
+        }
+        public void OnItemSetDefaults(Item item)
+        {
+            /*if (im)
+            {
+                item.consumable = false;
+            }
+            else
+            {
+                item.consumable = true;
+            }*/
+        }
+    }
+}
