99fe887c1841a770e0bb0c6564ffe4721445c092
MrBlueSL <tbecker1996@gmail.com>
2015-07-25 22:22:51 -0500

    Revamped, still needs fixed

--- Plugins/Consumable.cs
+++ Plugins/Consumable.cs
@@ -1,41 +1,50 @@
 using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using PluginLoader;
 using Terraria;
 using Terraria.ID;
 
 namespace MrBlueSLPlugins
 {
-    public class Consumable : MarshalByRefObject, IPluginChatCommand, IPluginItemSetDefaults
+    public class Consumable : MarshalByRefObject, IPluginItemSetDefaults, IPluginPlayerUpdate
     {
-        private bool im;
-        private bool i = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].consumable;
-        private string itm = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].name;
-        public bool OnChatCommand(string command, string[] args)
+        Color color = Color.Gold;
+        private bool con = false;
+        private Keys consumableKey;
+        public Consumable()
         {
-           /* string itm = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem].name;
-            if (command != "consumable") return false;
-            if (command == "consumable" && i == true)
-            {
-                im = false;
-                Main.NewText(itm + " will not consume on use");
-            }
-            else
-            {
-                im = true;
-                Main.NewText(itm + " will consume on use");
-            }*/
-            return true;
+            if (!Keys.TryParse(IniAPI.ReadIni("Consumable", "ConsumableKey", "B", writeIt: true), out consumableKey))
+                consumableKey = Keys.B;
+
+            Loader.RegisterHotkey(() =>
+                {
+                    con = !con;
+                    Main.NewText("Items will " + (con ? "not consume" : "consume") + " on use", color.R, color.G, color.B);
+                }, consumableKey);
         }
         public void OnItemSetDefaults(Item item)
         {
-            /*if (im)
+            Item i = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem];
+            Loader.RegisterHotkey(() =>
+                {
+                    if (i.consumable == false && con == true)
+                    {
+                        i.consumable = true;
+                    }
+                    else if (i.consumable == true && con == false)
+                    {
+                        i.consumable = false;
+                    }
+                }, consumableKey);
+        }
+        public void OnPlayerUpdate(Player player)
+        {   
+            Item i2 = Main.player[Main.myPlayer].inventory[Main.player[Main.myPlayer].selectedItem];
+            if (i2.pick > 0 || i2.axe > 0 || i2.hammer > 0 || i2.magic != false || i2.melee != false || i2.ranged != false)// saveguard against deleting weapons/tools?)
             {
-                item.consumable = false;
+                i2.consumable = false;
             }
-            else
-            {
-                item.consumable = true;
-            }*/
         }
     }
 }
